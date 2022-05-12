9cfdda04f1706825480b36de27361c5853d971d9
Sergey Zubkov <ksevelyar@protonmail.com>
2022-05-12 22:26:27 +0300

    rust: refactor difference of squares

--- rust/src/difference_of_squares.rs
+++ rust/src/difference_of_squares.rs
@@ -1,16 +1,9 @@
 fn square_of_sum(n: u32) -> u32 {
-    (0..n)
-        .enumerate()
-        .map(|(i, _)| i as u32 + 1)
-        .sum::<u32>()
-        .pow(2)
+    (0..n).map(|num| num + 1).sum::<u32>().pow(2)
 }
 
 fn sum_of_squares(n: u32) -> u32 {
-    (0..n)
-        .enumerate()
-        .map(|(i, _)| (i as u32 + 1).pow(2))
-        .sum::<u32>()
+    (0..n).map(|num| (num + 1).pow(2)).sum()
 }
 
 pub fn difference(n: u32) -> u32 {
