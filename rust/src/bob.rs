ae946ff0d2ba261ae6833db05c18011f84af3431
Sergey Zubkov <ksevelyar@protonmail.com>
2022-05-16 22:56:39 +0300

    add src/bob.rs

---
+++ rust/src/bob.rs
@@ -0,0 +1,18 @@
+fn is_yell(message: &str) -> bool {
+    let is_contain_uppercase = message.chars().any(|c| matches!(c, 'A'..='Z'));
+    let is_contain_lowercase = message.chars().any(|c| matches!(c, 'a'..='z'));
+
+    is_contain_uppercase && !is_contain_lowercase
+}
+
+pub fn reply(message: &str) -> &str {
+    let last_char = message.trim().chars().last();
+
+    match (last_char, is_yell(message)) {
+        (Some('?'), false) => "Sure.",
+        (Some('?'), true) => "Calm down, I know what I'm doing!",
+        (_, true) => "Whoa, chill out!",
+        (None, _) => "Fine. Be that way!",
+        _ => "Whatever.",
+    }
+}
