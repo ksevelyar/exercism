47945448cdd5cc68653019d1af7d0cc755c36282
Sergey Zubkov <ksevelyar@protonmail.com>
2022-05-16 22:52:02 +0300

    whatever

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,4 +1,5 @@
 mod armstrong_number;
+mod bob;
 mod difference_of_squares;
 mod reverse_string;
 
@@ -6,4 +7,5 @@ fn main() {
     dbg!(armstrong_number::is_armstrong_number(153));
     dbg!(reverse_string::reverse("hurma"));
     dbg!(difference_of_squares::difference(10));
+    dbg!(bob::reply("BABAH?"));
 }
