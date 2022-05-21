56e88f4956a14844ba2c06bb587b5c5ba8fcd7d3
Sergey Zubkov <ksevelyar@protonmail.com>
2022-05-21 16:12:29 +0300

    rust: solve beer song

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,20 +1,5 @@
-mod armstrong_number;
-mod bob;
-mod difference_of_squares;
-mod magazine_cutout;
-mod reverse_string;
+mod beer_song;
 
 fn main() {
-    dbg!(armstrong_number::is_armstrong_number(153));
-    dbg!(reverse_string::reverse("hurma"));
-    dbg!(difference_of_squares::difference(10));
-    dbg!(bob::reply("BABAH?"));
-
-    let magazine = "Astronomer Amy Mainzer spent hours chatting with Leonardo DiCaprio for Netflix's 'Don't Look Up'"
-        .split_whitespace()
-        .collect::<Vec<&str>>();
-    let note = "Amy Mainzer chatting with Leonardo DiCaprio"
-        .split_whitespace()
-        .collect::<Vec<&str>>();
-    dbg!(magazine_cutout::can_construct_note(&magazine, &note));
+    dbg!(beer_song::sing(3, 0));
 }
