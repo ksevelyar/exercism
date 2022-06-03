1899d2272e8a44416c9389b633283e47d8a966a9
Sergey Zubkov <ksevelyar@protonmail.com>
2022-06-04 01:27:47 +0300

    rust: solve matching brackets

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,11 +1,11 @@
 mod matching_brackets;
-mod proverb;
 
 fn main() {
     dbg!(matching_brackets::brackets_are_balanced("([][])"));
+
     dbg!(matching_brackets::brackets_are_balanced("(}{)"));
 
-    dbg!(proverb::build_proverb(&[
-        "nail", "shoe", "horse", "rider", "message", "battle", "kingdom"
-    ]));
+    dbg!(matching_brackets::brackets_are_balanced(")()"));
+
+    dbg!(matching_brackets::brackets_are_balanced("[(])"));
 }
--- rust/src/matching_brackets.rs
+++ rust/src/matching_brackets.rs
@@ -1,41 +1,42 @@
-fn is_start_bracket(char: &char) -> bool {
-    match char {
-        '(' | '{' | '[' => true,
-        _ => false,
-    }
+fn is_symmetry(string: &str) -> bool {
+    let unbalanced_brackets = string.chars().fold(
+        (0, 0, 0),
+        |(parenthesis, brace, bracket), char| match char {
+            '(' if parenthesis >= 0 => (parenthesis + 1, brace, bracket),
+            '{' if brace >= 0 => (parenthesis, brace + 1, bracket),
+            '[' if bracket >= 0 => (parenthesis, brace, bracket + 1),
+
+            ')' => (parenthesis - 1, brace, bracket),
+            '}' => (parenthesis, brace - 1, bracket),
+            ']' => (parenthesis, brace, bracket - 1),
+
+            _ => (parenthesis, brace, bracket),
+        },
+    );
+
+    unbalanced_brackets == (0, 0, 0)
 }
 
-fn is_end_bracket(char: &char) -> bool {
-    match char {
-        ')' | '}' | ']' => true,
+fn is_wrong_pattern(string: &str) -> bool {
+    let brackets = ['(', ')', '[', ']', '{', '}'];
+    let input_brackets = string.chars().filter(|char| brackets.contains(char));
+    // FIXME: remove mutation and clone
+    let mut paired_input_brackets = input_brackets.clone().zip(input_brackets.skip(1));
+    // FIXME: extract to external function
+    paired_input_brackets.any(|(prev, current)| match (prev, current) {
+        ('(', ']') => true,
+        ('(', '}') => true,
+
+        ('[', ')') => true,
+        ('[', '}') => true,
+
+        ('{', ']') => true,
+        ('{', ')') => true,
+
         _ => false,
-    }
-}
-
-fn compare_brackets(start_brackets: Vec<char>, end_brackets: Vec<char>) -> bool {
-    if start_brackets.len() == 0 || start_brackets.len() != end_brackets.len() {
-        return false;
-    }
-
-    let end_brackets_reverted = end_brackets.iter().rev().map(|bracket| match bracket {
-        ')' => '(',
-        ']' => '[',
-        '}' => '{',
-        _ => panic!("not a bracket"),
-    });
-
-    start_brackets
-        .iter()
-        .zip(end_brackets_reverted)
-        .all(|(a, b)| a == &b)
+    })
 }
 
 pub fn brackets_are_balanced(string: &str) -> bool {
-    let start_brackets = string
-        .chars()
-        .filter(|char| is_start_bracket(char))
-        .collect();
-    let end_brackets = string.chars().filter(|char| is_end_bracket(char)).collect();
-
-    compare_brackets(start_brackets, end_brackets)
+    !is_wrong_pattern(string) && is_symmetry(string)
 }
