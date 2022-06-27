6ab3e4d5545345d7989abb76a861ebcf752cb237
Sergey Zubkov <ksevelyar@protonmail.com>
2022-06-28 01:38:53 +0300

    rust: refactor collatz conjecture

--- rust/src/collatz_conjecture.rs
+++ rust/src/collatz_conjecture.rs
@@ -1,10 +1,13 @@
 fn transform(num: u64, count: u64) -> Option<u64> {
     match num {
         1 => return Some(count),
+
         num if num % 2 == 0 => transform(num / 2, count + 1),
-        num if num.checked_mul(3) == None => None,
-        num if (num * 3).checked_add(1) == None => None,
-        _ => transform(num * 3 + 1, count + 1),
+
+        num => match num.checked_mul(3)?.checked_add(1) {
+            None => None,
+            Some(int) => transform(int, count + 1),
+        },
     }
 }
 
