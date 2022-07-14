8f7d8aa8553ce96e7209e93e656493d55ad49a9a
Sergey Zubkov <ksevelyar@protonmail.com>
2022-07-15 00:03:25 +0300

    rust: add switch for sublist

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,5 @@
 mod medium;
 
 fn main() {
-  medium::sublist::greet();
+    dbg!(medium::sublist::sublist(&[1, 2, 3], &[1, 2, 3, 4, 5]));
 }
--- rust/src/medium/sublist.rs
+++ rust/src/medium/sublist.rs
@@ -1,3 +1,27 @@
-pub fn greet() {
-  println!("🐗 Sublist!");
+#[derive(Debug, PartialEq)]
+pub enum Comparison {
+    Equal,
+    Sublist,
+    Superlist,
+    Unequal,
+}
+
+fn is_equal<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
+    Comparison::Equal
+}
+
+fn is_superlist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
+    Comparison::Superlist
+}
+
+fn is_sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
+    Comparison::Sublist
+}
+
+pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
+    match (first_list.len(), second_list.len()) {
+        (first, last) if first > last => is_superlist(first_list, second_list),
+        (first, last) if first < last => is_sublist(first_list, second_list),
+        _ => is_equal(first_list, second_list),
+    }
 }
