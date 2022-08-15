24041227bcbcf91b3237cd0de56c0e4d52151b44
Sergey Zubkov <ksevelyar@protonmail.com>
2022-08-15 21:57:11 +0300

    rust: solve medium/anagram

--- rust/src/medium/anagram.rs
+++ rust/src/medium/anagram.rs
@@ -1,9 +1,27 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
-    );
+fn sorted_chars(word: &str) -> Vec<char> {
+    let mut chars: Vec<char> = word.chars().collect();
+    chars.sort_unstable();
+
+    chars
+}
+
+fn is_anagram(word: &str, candidate: &str) -> bool {
+    let downcased_candidate = candidate.to_lowercase();
+    if word == downcased_candidate {
+        return false;
+    }
+
+    sorted_chars(word) == sorted_chars(&downcased_candidate)
+}
+
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
+    let downcased_word = word.to_lowercase();
+
+    possible_anagrams
+        .iter()
+        .filter(|candidate| is_anagram(&downcased_word, candidate))
+        .copied()
+        .collect()
 }
