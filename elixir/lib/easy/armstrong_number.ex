037857cfaa2eb9add3398e0f748c6c3c1be6bf3c
Sergey Zubkov <ksevelyar@protonmail.com>
2022-08-21 14:33:08 +0300

    elixir: refactor armstrong numbers

--- elixir/lib/easy/armstrong_number.ex
+++ elixir/lib/easy/armstrong_number.ex
@@ -4,15 +4,13 @@ defmodule Easy.ArmstrongNumber do
   """
 
   defp sum_of_powered_digits(digits, pow) do
-    Enum.reduce(digits, 0, fn digit, sum -> sum + String.to_integer(digit) ** pow end)
+    Enum.reduce(digits, 0, fn digit, sum -> sum + digit ** pow end)
   end
 
-  @spec valid?(integer) :: boolean
   def valid?(number) do
-    number_string = Integer.to_string(number)
-    pow = String.length(number_string)
-    sum = String.graphemes(number_string) |> sum_of_powered_digits(pow)
+    digits = Integer.digits(number)
+    pow = length(digits)
 
-    sum == number
+    sum_of_powered_digits(digits, pow) == number
   end
 end
