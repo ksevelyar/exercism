2af5257f5f02a96f3f16237c239a42ba91671b24
Sergey Zubkov <ksevelyar@protonmail.com>
2022-09-01 09:33:46 +0300

    elixir: refactor Rational Numbers

--- elixir/lib/medium/rational_numbers.ex
+++ elixir/lib/medium/rational_numbers.ex
@@ -2,35 +2,23 @@ defmodule RationalNumbers do
   @type rational :: {integer, integer}
 
   @spec add(r1 :: rational, r2 :: rational) :: rational
-  def add({n1, d1}, {n2, d2}) do
-    {n1 * d2 + n2 * d1, d1 * d2}
-    |> reduce()
-  end
+  def add({n1, d1}, {n2, d2}), do: {n1 * d2 + n2 * d1, d1 * d2} |> reduce()
 
   @spec subtract(r1 :: rational, r2 :: rational) :: rational
-  def subtract({n1, d1}, {n2, d2}) do
-    {n1 * d2 - n2 * d1, d1 * d2}
-    |> reduce()
-  end
+  def subtract({n1, d1}, {n2, d2}), do: {n1 * d2 - n2 * d1, d1 * d2} |> reduce()
 
   @spec multiply(r1 :: rational, r2 :: rational) :: rational
-  def multiply({n1, d1}, {n2, d2}) do
-    {n1 * n2, d1 * d2}
-    |> reduce()
-  end
+  def multiply({n1, d1}, {n2, d2}), do: {n1 * n2, d1 * d2} |> reduce()
 
   @spec divide_by(num :: rational, den :: rational) :: rational
-  def divide_by({n1, d1}, {n2, d2}) do
-    {n1 * d2, d1 * n2}
-    |> reduce()
-  end
+  def divide_by({n1, d1}, {n2, d2}), do: {n1 * d2, d1 * n2} |> reduce()
 
   @spec abs(r :: rational) :: rational
-  def abs({n, d}), do: {Kernel.abs(n), Kernel.abs(d)}
+  def abs({n, d}), do: {Kernel.abs(n), Kernel.abs(d)} |> reduce()
 
   @spec pow_rational(r :: rational, pow :: integer) :: rational
-  def pow_rational({n, d}, pow) when pow >= 0, do: {n ** pow, d ** pow}
-  def pow_rational({n, d}, pow), do: {d ** -pow, n ** -pow}
+  def pow_rational({n, d}, pow) when pow >= 0, do: {n ** pow, d ** pow} |> reduce()
+  def pow_rational({n, d}, pow), do: {d ** -pow, n ** -pow} |> reduce()
 
   @spec pow_real(x :: integer, r :: rational) :: float
   def pow_real(x, {n, d}), do: x ** (n / d)
