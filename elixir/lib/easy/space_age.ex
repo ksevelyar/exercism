c8941b1ac98d719a08a3dfc3385383fe106f9f7b
Sergey Zubkov <ksevelyar@protonmail.com>
2022-09-02 10:17:35 +0300

    elixir: solve Space Age

---
+++ elixir/lib/easy/space_age.ex
@@ -0,0 +1,36 @@
+defmodule SpaceAge do
+  @type planet ::
+          :mercury
+          | :venus
+          | :earth
+          | :mars
+          | :jupiter
+          | :saturn
+          | :uranus
+          | :neptune
+
+  @periods %{
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    earth: 1,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+  }
+
+  @earth_year 31_557_600
+
+  @doc """
+  Return the number of years a person that has lived for 'seconds' seconds is
+  aged on 'planet', or an error if 'planet' is not a planet.
+  """
+  @spec age_on(planet, pos_integer) :: {:ok, float} | {:error, String.t()}
+  def age_on(planet, seconds) when is_map_key(@periods, planet) do
+    years = seconds / @earth_year
+    {:ok, years / @periods[planet]}
+  end
+
+  def age_on(_, _), do: {:error, "not a planet"}
+end
