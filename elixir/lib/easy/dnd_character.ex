638cb9918513c443bf42c8dd6a8059148b2b92e9
Sergey Zubkov <ksevelyar@protonmail.com>
2022-09-23 23:27:54 +0300

    elixir: solve DnD Character

---
+++ elixir/lib/easy/dnd_character.ex
@@ -0,0 +1,42 @@
+defmodule DndCharacter do
+  @type t :: %__MODULE__{
+          strength: pos_integer(),
+          dexterity: pos_integer(),
+          constitution: pos_integer(),
+          intelligence: pos_integer(),
+          wisdom: pos_integer(),
+          charisma: pos_integer(),
+          hitpoints: pos_integer()
+        }
+
+  defstruct ~w[strength dexterity constitution intelligence wisdom charisma hitpoints]a
+
+  @spec modifier(pos_integer()) :: integer()
+  def modifier(score), do: floor((score - 10) / 2)
+
+  @spec ability :: pos_integer()
+  def ability do
+    1..4
+    |> Enum.map(fn _ -> Enum.random(1..6) end)
+    |> Enum.sort()
+    |> Enum.take(-3)
+    |> Enum.sum()
+  end
+
+  @spec character :: t()
+  def character do
+    constitution = ability()
+
+    %__MODULE__{
+      strength: ability(),
+      dexterity: ability(),
+      constitution: constitution,
+      intelligence: ability(),
+      wisdom: ability(),
+      charisma: ability(),
+      hitpoints: hitpoints(constitution)
+    }
+  end
+
+  defp hitpoints(constitution), do: 10 + modifier(constitution)
+end
