2eceb21123cb84f35a48ff2068256313acd43e33
Sergey Zubkov <ksevelyar@protonmail.com>
2022-09-27 22:02:40 +0300

    rust/medium/minesweeper: bump neighbours()

--- rust/src/medium/minesweeper.rs
+++ rust/src/medium/minesweeper.rs
@@ -1,30 +1,31 @@
 use std::str;
 
-fn annotate_item(index: usize, item: u8, neighbours: [Option<&[u8]>; 2]) -> String {
+fn annotate_item(index: usize, item: u8, neighbours: &Vec<&[u8]>) -> String {
     let mine: u8 = 42;
     match item {
-      _ if item == mine => "*".to_owned(),
-      _ => "x".to_owned()
+        _ if item == mine => "*".to_owned(),
+        _ => "x".to_owned(),
     }
 }
 
-fn prev_row<'a>(index: usize, minefield: &'a [&str]) -> Option<&'a [u8]> {
-    match index {
-        0 => None,
-        _ => Some(minefield[index - 1].as_bytes()),
-    }
-}
-fn next_row<'a>(index: usize, minefield: &'a [&str]) -> Option<&'a [u8]> {
-    Some(minefield.get(index + 1)?.as_bytes())
+fn neighbours<'a>(index: usize, minefield: &'a [&str]) -> Vec<&'a [u8]> {
+    let prev_row = index.saturating_sub(1);
+    let next_row = match index {
+        _ if index == minefield.len() - 1 => index,
+        _ => index + 1,
+    };
+
+    let rows = &minefield[prev_row..=next_row];
+    dbg!(rows).iter().map(|row| row.as_bytes()).collect()
 }
 
 fn annotate_row(index: usize, minefield: &[&str]) -> String {
     let row = minefield[index].as_bytes();
-    let neighbours = [prev_row(index, minefield), next_row(index, minefield)];
+    let neighbours = neighbours(index, minefield);
 
     row.iter()
         .enumerate()
-        .map(|(index, item)| annotate_item(index, *item, neighbours))
+        .map(|(index, item)| annotate_item(index, *item, &neighbours))
         .collect()
 }
 
