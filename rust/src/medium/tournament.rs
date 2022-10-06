a08f8b8ebf3d29cc16735964b02b5bef03f04aa6
Sergey Zubkov <ksevelyar@protonmail.com>
2022-10-07 00:40:37 +0300

    rust/medium/tournament: add teams()

--- rust/src/medium/tournament.rs
+++ rust/src/medium/tournament.rs
@@ -1,5 +1,7 @@
+use std::collections::HashMap;
+
+#[derive(Debug)]
 struct Team {
-    name: String,
     matches_played: u32,
     matches_won: u32,
     matches_drawn: u32,
@@ -18,8 +20,34 @@ fn parse_matches(results: &str) -> Vec<Vec<&str>> {
         .collect()
 }
 
+fn teams(input: Vec<Vec<&str>>) -> HashMap<String, Team> {
+    input.iter().fold(HashMap::new(), |mut acc, result| {
+        let team1 = result[0];
+        let team2 = result[1];
+
+        acc.entry(team1.to_owned()).or_insert(Team {
+            matches_played: 0,
+            matches_won: 0,
+            matches_drawn: 0,
+            matches_lost: 0,
+            points: 0,
+        });
+
+        acc.entry(team2.to_owned()).or_insert(Team {
+            matches_played: 0,
+            matches_won: 0,
+            matches_drawn: 0,
+            matches_lost: 0,
+            points: 0,
+        });
+
+        acc
+    })
+}
+
 pub fn tally(match_results: &str) -> String {
-    dbg!(parse_matches(match_results));
+    let input = parse_matches(match_results);
+    dbg!(teams(input));
 
     header().to_string()
 }
