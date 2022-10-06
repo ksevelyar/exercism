44989de12ab06304ec874f1d1ac205cd9b5e8640
Sergey Zubkov <ksevelyar@protonmail.com>
2022-10-06 22:33:12 +0300

    rust/medium/tournament: add parse_matches()

--- rust/src/medium/tournament.rs
+++ rust/src/medium/tournament.rs
@@ -1,9 +1,25 @@
+struct Team {
+    name: String,
+    matches_played: u32,
+    matches_won: u32,
+    matches_drawn: u32,
+    matches_lost: u32,
+    points: u32,
+}
+
 fn header() -> &'static str {
     "Team                           | MP |  W |  D |  L |  P"
 }
 
+fn parse_matches(results: &str) -> Vec<Vec<&str>> {
+    results
+        .split("\n")
+        .map(|result| result.split(";").collect::<Vec<&str>>())
+        .collect()
+}
+
 pub fn tally(match_results: &str) -> String {
-    dbg!(match_results.split("\n"));
+    dbg!(parse_matches(match_results));
 
     header().to_string()
 }
