0e3eac6f3e499ac379a036d3879412dcd6c90f99
Sergey Zubkov <ksevelyar@protonmail.com>
2022-10-16 23:04:12 +0300

    solve rust/medium/space_age.rs

--- rust/src/medium/space_age.rs
+++ rust/src/medium/space_age.rs
@@ -1,4 +1,3 @@
-#[derive(Debug)]
 pub struct Duration {
     seconds: u64,
 }
@@ -19,55 +18,25 @@ pub trait Planet {
     }
 }
 
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
+macro_rules! planet {
+    ( $t:ident, $k:expr ) => {
+        pub struct $t;
+        impl Planet for $t {
+            fn year() -> f64 {
+                EARTH_YEAR * $k
+            }
+        }
+    };
+}
 
-impl Planet for Mercury {
-    fn year() -> f64 {
-        EARTH_YEAR * 0.2408467
-    }
-}
-impl Planet for Venus {
-    fn year() -> f64 {
-        EARTH_YEAR * 0.61519726
-    }
-}
-impl Planet for Earth {
-    fn year() -> f64 {
-        EARTH_YEAR * 1.0
-    }
-}
-impl Planet for Mars {
-    fn year() -> f64 {
-        EARTH_YEAR * 1.8808158
-    }
-}
-impl Planet for Jupiter {
-    fn year() -> f64 {
-        EARTH_YEAR * 11.862615
-    }
-}
-impl Planet for Saturn {
-    fn year() -> f64 {
-        EARTH_YEAR * 29.447498
-    }
-}
-impl Planet for Uranus {
-    fn year() -> f64 {
-        EARTH_YEAR * 84.016846
-    }
-}
-impl Planet for Neptune {
-    fn year() -> f64 {
-        EARTH_YEAR * 164.79132
-    }
-}
+planet!(Mercury, 0.2408467);
+planet!(Venus, 0.61519726);
+planet!(Earth, 1.0);
+planet!(Mars, 1.8808158);
+planet!(Jupiter, 11.862615);
+planet!(Saturn, 29.447498);
+planet!(Uranus, 84.016846);
+planet!(Neptune, 164.79132);
 
 #[cfg(test)]
 mod tests {
