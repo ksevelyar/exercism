2b711e651fd1e4bcfb394968fcaca99543d3c6d7
Sergey Zubkov <ksevelyar@protonmail.com>
2022-11-03 21:49:22 +0300

    solve rust/medium/word_count

--- rust/src/medium/word_count.rs
+++ rust/src/medium/word_count.rs
@@ -1,27 +1,29 @@
 use std::collections::HashMap;
 
 pub fn word_count(phrase: &str) -> HashMap<String, u32> {
-    let words: Vec<String> = phrase
-        .to_lowercase()
+    let lowercased = phrase.to_lowercase();
+    let words = lowercased
         .split(|ch: char| ch.is_whitespace() || ch.is_ascii_punctuation() && ch != '\'')
-        .filter(|x| *x != "")
-        .map(|x| x.to_owned())
-        .collect();
+        .filter(|word| !word.is_empty())
+        .map(|word| word.trim_matches('\''));
 
-    dbg!(&words);
-    HashMap::new()
+    words.fold(HashMap::new(), |mut map, word| {
+        *map.entry(word.to_owned()).or_insert(0) += 1;
+        map
+    })
 }
 
 #[test]
 fn with_apostrophes() {
-    let counts = word_count("First: don't laugh. Then: don't cry.");
+    let counts = word_count("Joe can't tell between 'large' and large.");
 
     let result = HashMap::from([
-        ("first".to_owned(), 1),
-        ("don't".to_owned(), 2),
-        ("laugh".to_owned(), 1),
-        ("then".to_owned(), 1),
-        ("cry".to_owned(), 1),
+        ("joe".to_owned(), 1),
+        ("can't".to_owned(), 1),
+        ("tell".to_owned(), 1),
+        ("between".to_owned(), 1),
+        ("large".to_owned(), 2),
+        ("and".to_owned(), 1),
     ]);
 
     assert_eq!(counts, result);
