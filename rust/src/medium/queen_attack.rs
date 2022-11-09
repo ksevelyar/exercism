23fc046e54fb3712ed670045b88a8b90308ff9e8
Sergey Zubkov <ksevelyar@protonmail.com>
2022-11-09 20:26:06 +0300

    solve rust/medium/queen_attack

--- rust/src/medium/queen_attack.rs
+++ rust/src/medium/queen_attack.rs
@@ -1,10 +1,9 @@
-#[derive(Debug)]
+#[derive(Eq, PartialEq)]
 pub struct ChessPosition {
     rank: i32,
     file: i32,
 }
 
-#[derive(Debug)]
 pub struct Queen {
     position: ChessPosition,
 }
@@ -28,24 +27,36 @@ impl Queen {
         Queen { position }
     }
 
-    fn check_diagonal_attacks(&self, other: &Queen) -> bool {
-        let rank = self.position.rank;
-        let file = self.position.file;
-
-        let top_left = if rank < file {
-            (0, file - rank)
-        } else {
-            (rank - file, 0)
-        };
-        dbg!(top_left);
-
-        let bottom_left = (rank + file, 0);
-        dbg!(bottom_left);
-        false
+    pub fn can_attack(&self, other: &Queen) -> bool {
+        self.can_attack_by_diagonal(other) || self.can_attack_by_rank_or_file(other)
     }
 
-    pub fn can_attack(&self, other: &Queen) -> bool {
-        self.check_diagonal_attacks(other)
+    fn diag_top_left(rank: i32, file: i32) -> ChessPosition {
+        if rank < file {
+            ChessPosition { file: 0, rank: file - rank }
+        } else {
+            ChessPosition { file: rank - file, rank: 0 }
+        }
+    }
+    fn diag_bottom_left(rank: i32, file: i32) -> ChessPosition {
+        if rank + file > 7 {
+            ChessPosition { file: 7, rank: 7 - rank + file }
+        } else {
+            ChessPosition { file: rank + file, rank: 0 }
+        }
+    }
+
+    fn can_attack_by_diagonal(&self, other: &Queen) -> bool {
+        let self_top_left = Queen::diag_top_left(self.position.rank, self.position.file);
+        let other_top_left = Queen::diag_top_left(other.position.rank, other.position.file);
+        let self_bottom_left = Queen::diag_bottom_left(self.position.rank, self.position.file);
+        let other_bottom_left = Queen::diag_bottom_left(other.position.rank, other.position.file);
+
+        self_top_left == other_top_left || self_bottom_left == other_bottom_left
+    }
+
+    fn can_attack_by_rank_or_file(&self, other: &Queen) -> bool {
+        self.position.rank == other.position.rank || self.position.file == other.position.file
     }
 }
 
@@ -54,6 +65,24 @@ fn queens_on_the_same_diagonal_can_attack() {
     let white_queen = Queen::new(ChessPosition::new(2, 2).unwrap());
 
     let black_queen = Queen::new(ChessPosition::new(0, 4).unwrap());
+
+    assert!(white_queen.can_attack(&black_queen));
+}
+
+#[test]
+fn queens_on_the_same_rank_can_attack() {
+    let white_queen = Queen::new(ChessPosition::new(2, 4).unwrap());
+
+    let black_queen = Queen::new(ChessPosition::new(2, 6).unwrap());
+
+    assert!(white_queen.can_attack(&black_queen));
+}
+
+#[test]
+fn queens_on_the_same_file_can_attack() {
+    let white_queen = Queen::new(ChessPosition::new(4, 5).unwrap());
+
+    let black_queen = Queen::new(ChessPosition::new(3, 5).unwrap());
 
     assert!(white_queen.can_attack(&black_queen));
 }
