b63f610f0728f323a922afd31f2b69de9e722227
Sergey Zubkov <ksevelyar@protonmail.com>
2022-11-14 18:58:10 +0300

    solve rust/medium/phone_number

--- rust/src/medium/phone_number.rs
+++ rust/src/medium/phone_number.rs
@@ -1,24 +1,29 @@
+const NANP_LEN: usize = 10;
+const ZONE_START: usize = 0;
+const SUBSCRIBER_START: usize = 3;
+const NOT_ALLOWED_FOR_START: [char; 2] = ['0', '1'];
+
+fn trim_chars(user_number: &str) -> Vec<char> {
+    let chars = user_number.chars().filter(|ch| ch.is_digit(10));
+
+    if chars.clone().count() == 11usize && chars.clone().next() == Some('1') {
+        chars.skip(1).collect()
+    } else {
+        chars.collect()
+    }
+}
+
+fn is_invalid_area_or_subscriber(area_start: char, subscriber_start: char) -> bool {
+    NOT_ALLOWED_FOR_START.contains(&area_start) || NOT_ALLOWED_FOR_START.contains(&subscriber_start)
+}
+
 pub fn number(user_number: &str) -> Option<String> {
-    let chars: Vec<char> = user_number
-        .chars()
-        .enumerate()
-        .filter(|(ind, ch)| ch.is_digit(10))
-        .map(|(_, ch)| ch)
-        .collect();
+    let chars = trim_chars(user_number);
+    let is_invalid = is_invalid_area_or_subscriber(chars[ZONE_START], chars[SUBSCRIBER_START]);
 
-    dbg!(&chars);
+    if chars.len() != NANP_LEN || is_invalid { return None }
 
-    if chars.len() < 10 || chars.len() > 11 {
-        return None;
-    }
-
-    if chars.len() == 11 {
-        if chars[0] != '1' {
-            return None;
-        }
-    }
-
-    Some(chars.iter().cloned().collect())
+    Some(chars.iter().collect())
 }
 
 #[test]
@@ -34,4 +39,9 @@ fn test_cleans_the_number_with_multiple_spaces() {
 #[test]
 fn test_invalid_if_area_code_starts_with_1() {
     assert_eq!(number("(123) 456-7890"), None);
+}
+
+#[test]
+fn test_invalid_when_11_digits_does_not_start_with_a_1() {
+    assert_eq!(number("22234567890"), None);
 }
