926a4b1f05d74f57ad181db3b5d8f94eb641d33c
Sergey Zubkov <ksevelyar@protonmail.com>
2022-12-17 00:31:00 +0300

    rust/medium/accumulate: rewrite with fold

--- rust/src/medium/accumulate.rs
+++ rust/src/medium/accumulate.rs
@@ -1,21 +1,11 @@
-pub fn map<F, T, D>(input: Vec<T>, fun: F) -> Vec<D>
+pub fn map<T, F, D>(input: Vec<T>, mut fun: F) -> Vec<D>
 where
     F: FnMut(T) -> D,
 {
-    map_with_acc(input, Vec::new(), fun)
-}
-
-fn map_with_acc<F, T, D>(mut input: Vec<T>, mut acc: Vec<D>, mut fun: F) -> Vec<D>
-where
-    F: FnMut(T) -> D,
-{
-    if input.is_empty() {
-        acc.reverse();
-        return acc;
-    }
-
-    acc.push(fun(input.pop().unwrap()));
-    map_with_acc(input, acc, fun)
+    input.into_iter().fold(Vec::new(), |mut acc, x| {
+        acc.push(fun(x));
+        acc
+    })
 }
 
 mod tests {
@@ -33,49 +23,49 @@ mod tests {
 
         assert_eq!(map(input, square), expected);
     }
-}
 
-#[test]
-fn strings() {
-    let input = vec!["1".to_string(), "2".into(), "3".into()];
+    #[test]
+    fn strings() {
+        let input = vec!["1".to_string(), "2".into(), "3".into()];
 
-    let expected = vec!["11".to_string(), "22".into(), "33".into()];
+        let expected = vec!["11".to_string(), "22".into(), "33".into()];
 
-    assert_eq!(map(input, |s| s.repeat(2)), expected);
-}
+        assert_eq!(map(input, |s| s.repeat(2)), expected);
+    }
 
-#[test]
-fn change_in_type() {
-    let input: Vec<&str> = vec!["1", "2", "3"];
+    #[test]
+    fn change_in_type() {
+        let input: Vec<&str> = vec!["1", "2", "3"];
 
-    let expected: Vec<String> = vec!["1".into(), "2".into(), "3".into()];
+        let expected: Vec<String> = vec!["1".into(), "2".into(), "3".into()];
 
-    assert_eq!(map(input, |s| s.to_string()), expected);
-}
+        assert_eq!(map(input, |s| s.to_string()), expected);
+    }
 
-#[test]
-fn minimal_bounds_on_input_and_output() {
-    struct Foo;
-    struct Bar;
+    #[test]
+    fn minimal_bounds_on_input_and_output() {
+        struct Foo;
+        struct Bar;
 
-    map(vec![Foo], |_| Bar);
-}
+        map(vec![Foo], |_| Bar);
+    }
 
-#[test]
-fn mutating_closure() {
-    let mut counter = 0;
+    #[test]
+    fn mutating_closure() {
+        let mut counter = 0;
 
-    let input = vec![-2, 3, 4, -5];
+        let input = vec![-2, 3, 4, -5];
 
-    let expected = vec![2, 3, 4, 5];
+        let expected = vec![2, 3, 4, 5];
 
-    let result = map(input, |x: i64| {
-        counter += 1;
+        let result = map(input, |x: i64| {
+            counter += 1;
 
-        x.abs()
-    });
+            x.abs()
+        });
 
-    assert_eq!(result, expected);
+        assert_eq!(result, expected);
 
-    assert_eq!(counter, 4);
+        assert_eq!(counter, 4);
+    }
 }
--- rust/src/medium/book_store.rs
+++ rust/src/medium/book_store.rs
@@ -1,5 +1,3 @@
-//! <https://exercism.org/tracks/rust/exercises/book-store>
-
 pub fn lowest_price(books: &[u32]) -> u32 {
     books.iter().map(|_book| 800).sum()
 }
@@ -7,4 +5,14 @@ pub fn lowest_price(books: &[u32]) -> u32 {
 #[test]
 fn test_only_a_single_book() {
     assert_eq!(lowest_price(&vec![1]), 800);
+}
+
+#[test]
+fn test_two_of_the_same_book() {
+    assert_eq!(lowest_price(&vec![2, 2]), 1_600);
+}
+
+#[test]
+fn test_two_different_books() {
+    assert_eq!(lowest_price(&vec![1, 2]), 1_520);
 }
