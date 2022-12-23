b3c32f79ace287ee8edd466b59de94946ff8521b
Sergey Zubkov <ksevelyar@protonmail.com>
2022-12-24 00:51:59 +0300

    solve rust/medium/grep

--- rust/src/medium/grep.rs
+++ rust/src/medium/grep.rs
@@ -1,29 +1,106 @@
 use anyhow::Error;
 use std::{
     fs::File,
-    io::{BufRead, BufReader, Read},
+    io::{BufRead, BufReader},
 };
 
-#[derive(Debug)]
-pub struct Flags<'a> {
-    flags: &'a [&'a str],
+#[derive(Debug, Default)]
+pub struct Flags {
+    line_numbers: bool,
+    only_names: bool,
+    case_insensitive: bool,
+    invert: bool,
+    entire_lines: bool,
 }
 
-impl<'a> Flags<'a> {
-    pub fn new(flags: &'a [&'a str]) -> Self {
-        Self { flags }
+impl Flags {
+    pub fn new(flags: &[&str]) -> Self {
+        flags
+            .iter()
+            .fold(Flags::default(), |mut flags, x| match *x {
+                "-n" => {
+                    flags.line_numbers = true;
+                    flags
+                }
+                "-l" => {
+                    flags.only_names = true;
+                    flags
+                }
+                "-i" => {
+                    flags.case_insensitive = true;
+                    flags
+                }
+                "-v" => {
+                    flags.invert = true;
+                    flags
+                }
+                "-x" => {
+                    flags.entire_lines = true;
+                    flags
+                }
+                _ => flags,
+            })
+    }
+}
+
+fn is_match(line: &str, pattern: &str, flags: &Flags) -> bool {
+    let is_match = match flags {
+        Flags {
+            entire_lines: true,
+            case_insensitive: true,
+            ..
+        } => line.to_lowercase() == (pattern.to_lowercase()),
+        Flags {
+            entire_lines: true,
+            case_insensitive: false,
+            ..
+        } => line == pattern,
+        Flags {
+            entire_lines: false,
+            case_insensitive: true,
+            ..
+        } => line.to_lowercase().contains(&pattern.to_lowercase()),
+        Flags {
+            entire_lines: false,
+            case_insensitive: false,
+            ..
+        } => line.contains(pattern),
+    };
+
+    if flags.invert {
+        !is_match
+    } else {
+        is_match
     }
 }
 
 pub fn grep(pattern: &str, flags: &Flags, files: &[&str]) -> Result<Vec<String>, Error> {
     let sources: Result<Vec<_>, Error> = files
         .iter()
-        .map(|file| {
-            let file = File::open(file)?;
+        .map(|file_name| {
+            let file = File::open(file_name)?;
             Ok(BufReader::new(file)
                 .lines()
                 .flatten()
-                .filter(|line| line.contains(pattern))
+                .enumerate()
+                .filter(|(_, line)| is_match(line, pattern, flags))
+                .map(|(ind, line)| {
+                    let line = if flags.line_numbers {
+                        format!("{}:{}", ind + 1, line)
+                    } else {
+                        line
+                    };
+
+                    if flags.only_names {
+                        return file_name.to_string();
+                    };
+
+                    if files.len() > 1 {
+                        format!("{}:{}", file_name, line)
+                    } else {
+                        line
+                    }
+                })
                 .collect::<Vec<String>>())
         })
         .filter(|d| match d.as_deref() {
@@ -32,8 +109,8 @@ pub fn grep(pattern: &str, flags: &Flags, files: &[&str]) -> Result<Vec<String>,
         })
         .collect();
 
-    match sources {
-        Ok(vec) => Ok(vec.into_iter().flatten().collect()),
-        Err(err) => Err(err),
-    }
+    let mut sources: Vec<_> = sources?.into_iter().flatten().collect();
+    sources.dedup();
+
+    Ok(sources.into_iter().collect())
 }
