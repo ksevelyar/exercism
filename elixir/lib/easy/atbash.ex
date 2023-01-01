63b13b13fece9f247acd85f07b967ab2cb118dad
Sergey Zubkov <ksevelyar@protonmail.com>
2023-01-02 00:01:28 +0300

    refactor elixir/easy/atbash

--- elixir/lib/easy/atbash.ex
+++ elixir/lib/easy/atbash.ex
@@ -1,5 +1,16 @@
 defmodule Atbash do
+  @encode_word_length 5
   @map Enum.zip(?a..?z, ?z..?a) |> Map.new()
+
+  def decode(cipher) do
+    chars =
+      cipher
+      |> String.downcase()
+      |> String.to_charlist()
+      |> Enum.filter(&(&1 != ?\s))
+
+    chars |> Enum.map(&(@map[&1] || &1)) |> Enum.reject(&is_nil/1) |> to_string()
+  end
 
   def encode(plaintext) do
     chars =
@@ -12,26 +23,13 @@ defmodule Atbash do
     for({ch, index} <- chars, !is_nil(ch), do: encode_char(ch, index)) |> to_string()
   end
 
-  def decode(cipher) do
-    chars =
-      cipher
-      |> String.downcase()
-      |> String.to_charlist()
-      |> Enum.filter(&(&1 != ?\s))
-
-    chars |> Enum.map(&decode_char/1) |> Enum.reject(&is_nil/1) |> to_string()
-  end
-
   defp encode_char(ch, index) do
-    encoded_char = @map[ch] || ch
+    maybe_encoded_char = @map[ch] || ch
+    insert_space? = index > 0 and rem(index, @encode_word_length) == 0
 
-    case rem(index, 5) do
-      0 when index > 0 -> [' ', encoded_char]
-      _ -> encoded_char
+    case insert_space? do
+      true -> [' ', maybe_encoded_char]
+      false -> maybe_encoded_char
     end
-  end
-
-  defp decode_char(ch) do
-    @map[ch] || ch
   end
 end
