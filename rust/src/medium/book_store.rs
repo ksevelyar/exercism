09d521b4b32226576890dd2ef1885e59e18ebc80
Sergey Zubkov <ksevelyar@protonmail.com>
2023-01-02 21:36:16 +0300

    solve rust/medium/book_store

--- rust/src/medium/book_store.rs
+++ rust/src/medium/book_store.rs
@@ -1,4 +1,5 @@
 const REGULAR_PRICE: u32 = 800;
+const BATCH_LIMIT: usize = 5;
 
 fn batch_price(num: usize) -> u32 {
     let discount = match num {
@@ -6,22 +7,37 @@ fn batch_price(num: usize) -> u32 {
         3 => REGULAR_PRICE / 100 * 10,
         4 => REGULAR_PRICE / 100 * 20,
         5 => REGULAR_PRICE / 100 * 25,
-        _ => 0
+        _ => 0,
     };
 
     (REGULAR_PRICE - discount) * num as u32
 }
 
+fn possible_sums(books: &[u32], limit: usize) -> u32 {
+    let batch = books
+        .iter()
+        .cloned()
+        .fold(Vec::new(), |mut sets: Vec<Vec<u32>>, book| {
+            let set = sets
+                .iter_mut()
+                .find(|set| !set.contains(&book) && set.len() < limit);
+
+            match set {
+                None => sets.push(vec![book]),
+                Some(set) => set.push(book),
+            };
+
+            sets
+        });
+
+    batch.iter().map(|batch| batch_price(batch.len())).sum()
+}
+
 pub fn lowest_price(books: &[u32]) -> u32 {
-    let batch = books.iter().cloned().fold(Vec::new(), |mut acc, book| {
-        if !acc.contains(&book) {
-            acc.push(book);
-        }
-
-        acc
-    });
-
-    batch_price(batch.len())
+    (2..=BATCH_LIMIT)
+        .map(|limit| possible_sums(books, limit))
+        .min()
+        .unwrap_or(0)
 }
 
 #[test]
@@ -57,4 +73,9 @@ fn test_five_different_books() {
 #[test]
 fn test_group_of_four_plus_group_of_two_is_cheaper_than_two_groups_of_three() {
     assert_eq!(lowest_price(&vec![1, 1, 2, 2, 3, 4]), 4_080);
+}
+
+#[test]
+fn test_two_groups_of_four_is_cheaper_than_group_of_five_plus_group_of_three() {
+    assert_eq!((lowest_price(&vec![1, 1, 2, 2, 3, 3, 4, 5])), 5_120,);
 }
