d1ff55e147dde6f4c3e8ae8b10fdf47a4bcf5768
Sergey Zubkov <ksevelyar@protonmail.com>
2023-01-20 01:02:48 +0300

    rust/medium/say: iter over thousands

--- rust/src/medium/say.rs
+++ rust/src/medium/say.rs
@@ -1,77 +1,77 @@
-use std::fmt::format;
-
 pub fn encode(n: u64) -> String {
     let num_string = n.to_string();
     let length = num_string.len() - 1;
 
-    if n > 100 {
-    } else {
-    }
+    let vec = num_string.chars().rev().collect::<Vec<_>>();
+    let thousands = vec.chunks(3).rev().collect::<Vec<_>>();
 
-    num_string
-        .char_indices()
-        .map(|(ind, ch)| {
-            say_factor(
-                ch.to_digit(10).expect("u32").into(),
+    thousands
+        .into_iter()
+        .enumerate()
+        .map(|(ind, chars)| {
+            say_number(
+                chars.iter().rev().copied().collect::<Vec<_>>().as_slice(),
                 10u64.pow((length - ind) as u32),
             )
         })
-        .collect()
+        .collect::<Vec<_>>()
+        .join(" ")
 }
 
-fn say_number(number: u64) -> String {
-    match number {
-        1 => "one".to_string(),
-        10 => "ten".to_string(),
-        100 => "hundred".to_string(),
-        1000 => "thousand".to_string(),
-        1000000 => "million".to_string(),
-        1000_000_000 => "billion".to_string(),
-        1000_000_000_000 => "trillion".to_string(),
-        0 => "zero".to_string(),
-        2 => "two".to_string(),
-        3 => "three".to_string(),
-        4 => "four".to_string(),
-        5 => "five".to_string(),
-        6 => "six".to_string(),
-        7 => "seven".to_string(),
-        8 => "eight".to_string(),
-        9 => "nine".to_string(),
-        11 => "eleven".to_string(),
-        12 => "twelve".to_string(),
-        13 => "thirteen".to_string(),
-        14 => "fourteen".to_string(),
-        15 => "fifteen".to_string(),
-        16 => "sixteen".to_string(),
-        17 => "seventeen".to_string(),
-        18 => "eighteen".to_string(),
-        19 => "nineteen".to_string(),
-        20 => "twenty".to_string(),
-        21..=29 => format!("twenty-{}", number - 20),
-        30 => "thirty".to_string(),
-        31..=39 => format!("thirty-"),
-        40 => "forty".to_string(),
-        41..=49 => format!("forty-{}", number - 40),
-        50 => "fifty".to_string(),
-        51..=59 => format!("fifty-{}", number - 50),
-        60 => "sixty".to_string(),
-        61..=69 => format!("sixty-{}", number - 60),
-        70 => "seventy".to_string(),
-        71..=79 => format!("seventy-{}", number - 70),
-        80 => "eighty".to_string(),
-        81..=89 => format!("eighty-{}", number - 80),
-        90 => "ninety".to_string(),
-        91..=99 => format!("ninety-{}", number - 90),
-        _ => "🐗".to_string(),
-    }
-    .to_string()
+fn say_number(chars: &[char], number: u64) -> String {
+    let count = match chars {
+        ['1'] => "one".to_string(),
+        ['0'] => "zero".to_string(),
+        ['2'] => "two".to_string(),
+        ['3'] => "three".to_string(),
+        ['4'] => "four".to_string(),
+        ['5'] => "five".to_string(),
+        ['6'] => "six".to_string(),
+        ['7'] => "seven".to_string(),
+        ['8'] => "eight".to_string(),
+        ['9'] => "nine".to_string(),
+        ['1', '0'] => "ten".to_string(),
+        ['1', '1'] => "eleven".to_string(),
+        ['1', '2'] => "twelve".to_string(),
+        ['1', '3'] => "thirteen".to_string(),
+        ['1', '4'] => "fourteen".to_string(),
+        ['1', '5'] => "fifteen".to_string(),
+        ['1', '6'] => "sixteen".to_string(),
+        ['1', '7'] => "seventeen".to_string(),
+        ['1', '8'] => "eighteen".to_string(),
+        ['1', '9'] => "nineteen".to_string(),
+        ['2', '0'] => "twenty".to_string(),
+        ['2', n] => format!("twenty-{}", say_number(&[*n], 0)),
+        ['3', n] => format!("thirty-{}", say_number(&[*n], 0)),
+        ['4', n] => format!("forty-{}", say_number(&[*n], 0)),
+        ['5', n] => format!("fifty-{}", say_number(&[*n], 0)),
+        ['6', n] => format!("sixty-{}", say_number(&[*n], 0)),
+        ['7', n] => format!("seventy-{}", say_number(&[*n], 0)),
+        ['8', n] => format!("eighty-{}", say_number(&[*n], 0)),
+        ['9', n] => format!("ninety-{}", say_number(&[*n], 0)),
+        ['0', '0'] => format!("").to_string(),
+        [n, a, b] => format!(
+            "{} hundred {}",
+            say_number(&[*n], 100),
+            say_number(&[*a, *b], 0)
+        ),
+        _ => {
+            dbg!(chars);
+            "dbg!".to_string()
+        }
+    };
+
+    format!("{}{}", count, say_thouthands(number))
 }
 
-fn say_factor(count: u64, factor: u64) -> String {
-    match (count, factor) {
-        (_, 10) => say_number(count * factor),
-        (_, 1) => format!("-{}", say_number(count)),
-        _ => format!("{} {} ", say_number(count), say_number(factor)),
+fn say_thouthands(count: u64) -> String {
+    dbg!(count);
+    match count {
+        1000 => " thousand".to_string(),
+        1000_000 => " million".to_string(),
+        1000_000_000 => " billion".to_string(),
+        1000_000_000_000 => " trillion".to_string(),
+        _ => "".to_string(),
     }
 }
 
