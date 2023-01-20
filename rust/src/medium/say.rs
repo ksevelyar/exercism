5e9f8f30ed909be6606a7a9159a06941fe6e1ced
Sergey Zubkov <ksevelyar@protonmail.com>
2023-01-20 23:20:38 +0300

    rust/medium/say: test_one_million_two()

--- rust/src/medium/say.rs
+++ rust/src/medium/say.rs
@@ -9,19 +9,30 @@ pub fn encode(n: u64) -> String {
         .into_iter()
         .enumerate()
         .map(|(ind, chars)| {
-            say_number(
-                chars.iter().rev().copied().collect::<Vec<_>>().as_slice(),
-                10u64.pow((length - ind) as u32),
-            )
+            let chars = chars.iter().rev().copied().collect::<Vec<_>>();
+            let power = 10u64.pow((length - ind) as u32);
+
+            dbg!(&chars);
+            match chars.as_slice() {
+                [a, '0', '0'] => format!("{}{}", say_number(&[*a]), say_thousands(power)),
+                [a, b, c] => format!(
+                    "{}{} {}",
+                    say_number(&[*a]),
+                    say_thousands(power),
+                    say_number(&[*b, *c])
+                ),
+                ['0'] => "zero".to_string(),
+                _ => format!("{}{}", say_number(&chars), say_thousands(power)),
+            }
         })
+        .filter(|x| dbg!(x) != " " && x != "")
         .collect::<Vec<_>>()
         .join(" ")
 }
 
-fn say_number(chars: &[char], number: u64) -> String {
-    let count = match chars {
+fn say_number(chars: &[char]) -> String {
+    match chars {
         ['1'] => "one".to_string(),
-        ['0'] => "zero".to_string(),
         ['2'] => "two".to_string(),
         ['3'] => "three".to_string(),
         ['4'] => "four".to_string(),
@@ -41,32 +52,23 @@ fn say_number(chars: &[char], number: u64) -> String {
         ['1', '8'] => "eighteen".to_string(),
         ['1', '9'] => "nineteen".to_string(),
         ['2', '0'] => "twenty".to_string(),
-        ['2', n] => format!("twenty-{}", say_number(&[*n], 0)),
-        ['3', n] => format!("thirty-{}", say_number(&[*n], 0)),
-        ['4', n] => format!("forty-{}", say_number(&[*n], 0)),
-        ['5', n] => format!("fifty-{}", say_number(&[*n], 0)),
-        ['6', n] => format!("sixty-{}", say_number(&[*n], 0)),
-        ['7', n] => format!("seventy-{}", say_number(&[*n], 0)),
-        ['8', n] => format!("eighty-{}", say_number(&[*n], 0)),
-        ['9', n] => format!("ninety-{}", say_number(&[*n], 0)),
-        ['0', '0'] => format!("").to_string(),
-        [n, a, b] => format!(
-            "{} hundred {}",
-            say_number(&[*n], 100),
-            say_number(&[*a, *b], 0)
-        ),
-        _ => {
-            dbg!(chars);
-            "dbg!".to_string()
-        }
-    };
-
-    format!("{}{}", count, say_thouthands(number))
+        ['2', n] => format!("twenty-{}", say_number(&[*n])),
+        ['3', n] => format!("thirty-{}", say_number(&[*n])),
+        ['4', n] => format!("forty-{}", say_number(&[*n])),
+        ['5', n] => format!("fifty-{}", say_number(&[*n])),
+        ['6', n] => format!("sixty-{}", say_number(&[*n])),
+        ['7', n] => format!("seventy-{}", say_number(&[*n])),
+        ['8', n] => format!("eighty-{}", say_number(&[*n])),
+        ['9', n] => format!("ninety-{}", say_number(&[*n])),
+        ['0', '0'] => "".to_string(),
+        ['0', n] => say_number(&[*n]),
+        _ => format!(""),
+    }
 }
 
-fn say_thouthands(count: u64) -> String {
-    dbg!(count);
+fn say_thousands(count: u64) -> String {
     match count {
+        100 => " hundred".to_string(),
         1000 => " thousand".to_string(),
         1000_000 => " million".to_string(),
         1000_000_000 => " billion".to_string(),
@@ -111,4 +113,9 @@ fn test_one_thousand_two_hundred_thirty_four() {
         encode(1234),
         String::from("one thousand two hundred thirty-four")
     );
+}
+
+#[test]
+fn test_one_million_two() {
+    assert_eq!(encode(1_000_002), String::from("one million two"));
 }
