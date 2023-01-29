4589bb1b7724279792b05175b3f84af1f14c304b
Sergey Zubkov <ksevelyar@protonmail.com>
2023-01-29 22:45:31 +0300

    rust/medium/two_buckets: pour_from_small_to_big()

--- rust/src/medium/two_buckets.rs
+++ rust/src/medium/two_buckets.rs
@@ -4,32 +4,51 @@ pub enum Bucket {
     Two,
 }
 
-/// A struct to hold your results in.
 #[derive(PartialEq, Eq, Debug)]
 pub struct BucketStats {
-    /// The total number of "moves" it should take to reach the desired number of liters, including
-    /// the first fill.
     pub moves: u8,
-    /// Which bucket should end up with the desired number of liters? (Either "one" or "two")
     pub goal_bucket: Bucket,
-    /// How many liters are left in the other bucket?
     pub other_bucket: u8,
 }
 
-/// Solve the bucket problem
+// NOTE opposite starting point disallowed
+fn pour_from_big_to_small() -> Option<BucketStats> {
+    let moves = 0;
+    let other_bucket = 0;
+
+    Some(BucketStats {
+        moves,
+        goal_bucket: Bucket::Two,
+        other_bucket,
+    })
+}
+
+fn pour_from_small_to_big() -> Option<BucketStats> {
+    let moves = 42;
+    let other_bucket = 42;
+
+    Some(BucketStats {
+        moves,
+        goal_bucket: Bucket::One,
+        other_bucket,
+    })
+}
+
 pub fn solve(
     capacity_1: u8,
     capacity_2: u8,
     goal: u8,
     start_bucket: &Bucket,
 ) -> Option<BucketStats> {
-    unimplemented!(
-        "Given one bucket of capacity {}, another of capacity {}, starting with {:?}, find pours to reach {}, or None if impossible",
-        capacity_1,
-        capacity_2,
-        start_bucket,
-        goal,
-    );
+    let is_small_to_big_direction = match start_bucket {
+        Bucket::One => capacity_1 < capacity_2,
+        Bucket::Two => capacity_2 < capacity_1,
+    };
+
+    match is_small_to_big_direction {
+        true => pour_from_small_to_big(),
+        false => pour_from_big_to_small(),
+    }
 }
 
 #[test]
