9d946699626412121b21107ebeed27d0e8dfc0a6
Sergey Zubkov <ksevelyar@protonmail.com>
2023-02-05 14:53:45 +0300

    solve rust/medium/custom_set

--- rust/src/medium/custom_set.rs
+++ rust/src/medium/custom_set.rs
@@ -7,6 +7,8 @@ impl<T: std::clone::Clone + std::cmp::PartialEq + std::cmp::Ord> CustomSet<T> {
     pub fn new(input: &[T]) -> Self {
         let mut state = input.to_vec();
         state.sort_unstable();
+        state.dedup();
+
         CustomSet { state }
     }
 
@@ -33,19 +35,37 @@ impl<T: std::clone::Clone + std::cmp::PartialEq + std::cmp::Ord> CustomSet<T> {
         self.state.iter().all(|item| !other.contains(item))
     }
 
-    #[must_use]
-    pub fn intersection(&self, _other: &Self) -> Self {
-        unimplemented!();
+    pub fn intersection(&self, other: &Self) -> Self {
+        Self::new(
+            &self
+                .state
+                .iter()
+                .filter(|item| other.contains(item))
+                .cloned()
+                .collect::<Vec<_>>(),
+        )
     }
 
-    #[must_use]
-    pub fn difference(&self, _other: &Self) -> Self {
-        unimplemented!();
+    pub fn difference(&self, other: &Self) -> Self {
+        Self::new(
+            &self
+                .state
+                .iter()
+                .filter(|item| !other.contains(item))
+                .cloned()
+                .collect::<Vec<_>>(),
+        )
     }
 
-    #[must_use]
-    pub fn union(&self, _other: &Self) -> Self {
-        unimplemented!();
+    pub fn union(&self, other: &Self) -> Self {
+        Self::new(
+            &self
+                .state
+                .iter()
+                .chain(other.state.iter())
+                .cloned()
+                .collect::<Vec<_>>(),
+        )
     }
 }
 
@@ -65,4 +85,15 @@ fn set_contained_in_other_set_is_a_subset() {
     let set2 = CustomSet::new(&[4, 1, 2, 3]);
 
     assert!(set1.is_subset(&set2));
+}
+
+#[test]
+fn intersection_of_two_sets_with_shared_elements_is_a_set_of_the_shared_elements() {
+    let set1 = CustomSet::new(&[1, 2, 3, 4]);
+
+    let set2 = CustomSet::new(&[3, 2, 5]);
+
+    assert_eq!(set1.intersection(&set2), CustomSet::new(&[2, 3]));
+
+    assert_eq!(set2.intersection(&set1), CustomSet::new(&[2, 3]));
 }
