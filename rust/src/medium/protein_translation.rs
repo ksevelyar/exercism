0846764f47c4a39213ea84441d0d45fa70198405
Sergey Zubkov <ksevelyar@protonmail.com>
2023-03-11 16:04:41 +0300

    solve rust/medium/protein_translation

--- rust/src/medium/protein_translation.rs
+++ rust/src/medium/protein_translation.rs
@@ -1,23 +1,32 @@
 pub struct CodonsInfo<'a> {
-    // We fake using 'a here, so the compiler does not complain that
-    // "parameter `'a` is never used". Delete when no longer needed.
-    phantom: std::marker::PhantomData<&'a ()>,
+    pairs: Vec<(&'a str, &'a str)>,
 }
 
 impl<'a> CodonsInfo<'a> {
     pub fn name_for(&self, codon: &str) -> Option<&'a str> {
-        unimplemented!(
-            "Return the protein name for a '{codon}' codon or None, if codon string is invalid"
-        );
+        self.pairs
+            .iter()
+            .find(|(stored_codon, _protein)| codon == *stored_codon)
+            .map(|(_, protein)| *protein)
     }
 
     pub fn of_rna(&self, rna: &str) -> Option<Vec<&'a str>> {
-        unimplemented!("Return a list of protein names that correspond to the '{rna}' RNA string or None if the RNA string is invalid");
+        let codons_count = match rna.len() % 3 {
+            0 => rna.len() / 3,
+            _ => rna.len() / 3 + 1,
+        };
+
+        let codons: Option<Vec<&str>> = (0..codons_count)
+            .map(|ind| rna.get((ind * 3)..(ind * 3 + 3)))
+            .take_while(|codon| self.name_for(codon.unwrap_or("")) != Some("stop codon"))
+            .collect();
+
+        codons?.iter().map(|codon| self.name_for(codon)).collect()
     }
 }
 
 pub fn parse<'a>(pairs: Vec<(&'a str, &'a str)>) -> CodonsInfo<'a> {
-    unimplemented!("Construct a new CodonsInfo struct from given pairs: {pairs:?}");
+    CodonsInfo { pairs }
 }
 
 mod tests {
@@ -64,5 +73,39 @@ mod tests {
     fn test_methionine() {
         let info = parse(make_pairs());
         assert_eq!(info.name_for("AUG"), Some("methionine"));
+    }
+
+    #[test]
+    fn test_translates_rna_strand_into_correct_protein() {
+        let info = parse(make_pairs());
+
+        assert_eq!(
+            info.of_rna("AUGUUUUGG"),
+            Some(vec!["methionine", "phenylalanine", "tryptophan"])
+        );
+    }
+
+    #[test]
+    fn test_stops_translation_if_stop_codon_present() {
+        let info = parse(make_pairs());
+
+        assert_eq!(
+            info.of_rna("AUGUUUUAA"),
+            Some(vec!["methionine", "phenylalanine"])
+        );
+    }
+
+    #[test]
+    fn test_invalid_length() {
+        let info = parse(make_pairs());
+
+        assert!(info.of_rna("AUGUA").is_none());
+    }
+
+    #[test]
+    fn test_valid_stopped_rna() {
+        let info = parse(make_pairs());
+
+        assert_eq!(info.of_rna("AUGUAAASDF"), Some(vec!["methionine"]));
     }
 }
