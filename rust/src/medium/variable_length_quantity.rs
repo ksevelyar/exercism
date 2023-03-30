a4a784b525c266242d217d21926016d3deea0008
Sergey Zubkov <ksevelyar@protonmail.com>
2023-03-30 20:56:50 +0300

    rust/medium/variable_length_quantity: from_triple_byte()

--- rust/src/medium/variable_length_quantity.rs
+++ rust/src/medium/variable_length_quantity.rs
@@ -9,15 +9,23 @@ pub fn to_bytes(values: &[u32]) -> Vec<u8> {
 }
 
 pub fn from_bytes(bytes: &[u8]) -> Result<Vec<u32>, Error> {
-    println!("{:b}", 8192);
-    let sum: u32 = bytes.iter().enumerate().rev().fold(0u32, |acc, (ind, byte)| {
-        dbg!(byte, ind);
-        println!("{:b}", byte);
-        println!("{:b}", (byte & 0b1111111) >> (7 * ind));
+    println!("goal: {:b}", 106903);
+    dbg!(bytes);
 
-        acc + acc | ((byte >> (7 * ind)) & 0b1111111) as u32
-    });
-    println!("{:b}", sum);
+    let sum: u32 = bytes
+        .iter()
+        .rev()
+        .enumerate()
+        .fold(0u32, |acc, (ind, byte)| {
+            dbg!(byte, ind);
+            println!("byte {:07b}\n", (byte & 0b1111111));
+
+            let shift = ((byte & 0b1111111) as u32) << (7 * ind);
+            println!("shift {:07b}\n", shift);
+
+            acc | shift
+        });
+    println!("result: {:b}", sum);
 
     Ok(vec![sum])
 }
@@ -85,17 +93,22 @@ fn to_bytes_multiple_values() {
 }
 
 #[test]
-fn from_bytes_specs() {
-    // assert_eq!(Ok(vec![0x7f]), from_bytes(&[0x7f]));
+fn from_triple_byte() {
+    assert_eq!(Ok(vec![106903]), from_bytes(&[134, 195, 23]));
+}
+
+#[test]
+fn from_various_bytes() {
+    assert_eq!(Ok(vec![0x7f]), from_bytes(&[0x7f]));
 
     assert_eq!(Ok(vec![0x2000]), from_bytes(&[0xc0, 0x00]));
-    //
-    // assert_eq!(Ok(vec![0x1f_ffff]), from_bytes(&[0xff, 0xff, 0x7f]));
-    //
-    // assert_eq!(Ok(vec![0x20_0000]), from_bytes(&[0x81, 0x80, 0x80, 0x00]));
-    //
-    // assert_eq!(
-    //     Ok(vec![0xffff_ffff]),
-    //     from_bytes(&[0x8f, 0xff, 0xff, 0xff, 0x7f])
-    // );
+
+    assert_eq!(Ok(vec![0x1f_ffff]), from_bytes(&[0xff, 0xff, 0x7f]));
+
+    assert_eq!(Ok(vec![0x20_0000]), from_bytes(&[0x81, 0x80, 0x80, 0x00]));
+
+    assert_eq!(
+        Ok(vec![0xffff_ffff]),
+        from_bytes(&[0x8f, 0xff, 0xff, 0xff, 0x7f])
+    );
 }
