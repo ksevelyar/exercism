350328dc5956c434e7401acbcad7fdefd2935043
Sergey Zubkov <ksevelyar@protonmail.com>
2023-06-11 21:32:51 +0300

    add elixir/easy/paint_by_number

---
+++ elixir/lib/easy/paint_by_number.ex
@@ -0,0 +1,36 @@
+defmodule PaintByNumber do
+  def palette_bit_size(color_count) do
+    find_enough_bytes(0, color_count)
+  end
+
+  defp find_enough_bytes(byte_count, color_count) do
+    case Integer.pow(2, byte_count) >= color_count do
+      true -> byte_count
+      false -> find_enough_bytes(byte_count + 1, color_count)
+    end
+  end
+
+  def empty_picture() do
+    # Please implement the empty_picture/0 function
+  end
+
+  def test_picture() do
+    # Please implement the test_picture/0 function
+  end
+
+  def prepend_pixel(picture, color_count, pixel_color_index) do
+    # Please implement the prepend_pixel/3 function
+  end
+
+  def get_first_pixel(picture, color_count) do
+    # Please implement the get_first_pixel/2 function
+  end
+
+  def drop_first_pixel(picture, color_count) do
+    # Please implement the drop_first_pixel/2 function
+  end
+
+  def concat_pictures(picture1, picture2) do
+    # Please implement the concat_pictures/2 function
+  end
+end
