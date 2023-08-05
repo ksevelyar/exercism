5facea2cb9328dbf92623e3fbb64493efd53291c
Sergey Zubkov <ksevelyar@protonmail.com>
2023-08-05 15:24:37 +0300

    elixir/easy/pig_latin: maybe_move_consonant_cluster()

--- elixir/lib/easy/pig_latin.ex
+++ elixir/lib/easy/pig_latin.ex
@@ -3,17 +3,34 @@ defmodule PigLatin do
   Given a `phrase`, translate it a word at a time to Pig Latin.
   """
   @vowels ~w(a e i o u)
+  @vowels_at_start ~w(xr yt)
 
   @spec translate(phrase :: String.t()) :: String.t()
   def translate(phrase) do
-    maybe_add_yay(phrase)
+    if String.starts_with?(phrase, @vowels ++ @vowels_at_start) do
+      "#{phrase}ay"
+    else
+      "#{maybe_move_consonant_cluster(phrase)}ay"
+    end
   end
 
-  defp maybe_add_yay(word) do
-    if String.starts_with?(word, @vowels ++ ["xr", "yt"]) do
-      "#{word}ay"
+  def maybe_move_consonant_cluster(word) do
+    move_consonant_cluster(word, "")
+  end
+
+  defp move_consonant_cluster(<<>>, acc) do
+    acc
+  end
+
+  defp move_consonant_cluster(<<"qu", rest::binary>>, acc) do
+    move_consonant_cluster(rest, acc <> "qu")
+  end
+
+  defp move_consonant_cluster(<<head::bitstring-size(8), rest::binary>>, acc) do
+    if head in @vowels do
+      move_consonant_cluster(<<>>, head <> rest <> acc)
     else
-      word
+      move_consonant_cluster(rest, acc <> head)
     end
   end
 end
