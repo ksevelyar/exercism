21fb5c335e0885e822b3b0dbb2307f0ec47bf1c4
Sergey Zubkov <ksevelyar@protonmail.com>
2023-08-21 21:52:49 +0300

    solve elixir/easy/run_length_encoder

--- elixir/lib/easy/run_length_encoder.ex
+++ elixir/lib/easy/run_length_encoder.ex
@@ -7,44 +7,35 @@ defmodule RunLengthEncoder do
   "2A3B4C" => "AABBBCCCC"
   """
   @spec encode(String.t()) :: String.t()
-  def encode(string) do
-    do_encode(string, 0, nil, "")
-  end
+  def encode(string), do: do_encode(string, 1, nil, "")
 
   defp do_encode(<<>>, count, letter, acc) do
-    count = if count == 1 or letter == nil, do: "", else: count
+    count = if count > 1, do: count
 
     "#{acc}#{count}#{letter}"
   end
-
-  defp do_encode(<<current_letter::bitstring-size(8), rest::binary>>, count, letter, acc)
+  defp do_encode(<<current_letter::binary-size(1), rest::binary>>, count, letter, acc)
        when current_letter == letter do
     do_encode(rest, count + 1, letter, acc)
   end
-
-  defp do_encode(<<current_letter::bitstring-size(8), rest::binary>>, 0, nil, acc) do
-    do_encode(rest, 1, current_letter, acc)
-  end
-
-  defp do_encode(<<current_letter::bitstring-size(8), rest::binary>>, count, letter, acc) do
-    count = if count == 1, do: "", else: count
+  defp do_encode(<<current_letter::binary-size(1), rest::binary>>, count, letter, acc) do
+    count = if count > 1, do: count
 
     do_encode(rest, 1, current_letter, "#{acc}#{count}#{letter}")
   end
 
   @spec decode(String.t()) :: String.t()
-  def decode(string) do
-    do_decode(string, 0, "")
-  end
+  def decode(string), do: do_decode(string, 1, "")
 
-  defp do_decode(<<>>, _count, acc) do
-    acc
-  end
+  defp do_decode(<<>>, _count, acc), do: acc
+  defp do_decode(string, count, acc) do
+    case Integer.parse(string) do
+      :error ->
+        <<letter::binary-size(1)>> <> rest = string
+        do_decode(rest, 1, acc <> String.duplicate(letter, count))
 
-  defp do_decode(<<current_letter::bitstring-size(8), rest::binary>>, count, acc) do
-    case Integer.parse(current_letter) do
-      :error -> do_decode(rest, 0, "#{acc}#{String.duplicate(current_letter, count)}")
-      {number, _} -> do_decode(rest, number, acc)
+      {number, rest} ->
+        do_decode(rest, number, acc)
     end
   end
 end
