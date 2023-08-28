38529fe1f32df354efea51dd17c7166bf7219b20
Sergey Zubkov <ksevelyar@protonmail.com>
2023-08-28 22:44:33 +0300

    solve elixir/easy/gigasecond

---
+++ elixir/lib/easy/gigasecond.ex
@@ -0,0 +1,20 @@
+defmodule Gigasecond do
+  @doc """
+  Calculate a date one billion seconds after an input date.
+  """
+  @spec from({{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}) ::
+          {{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}
+  def from({{year, month, day}, {hours, minutes, seconds}}) do
+    from = %NaiveDateTime{
+      year: year,
+      month: month,
+      day: day,
+      hour: hours,
+      minute: minutes,
+      second: seconds
+    }
+    result = NaiveDateTime.add(from, 1_000_000_000)
+
+    {{result.year, result.month, result.day}, {result.hour, result.minute, result.second}}
+  end
+end
