d86c3323bb5fa0631be5144e946973bf1833cf50
Sergey Zubkov <ksevelyar@protonmail.com>
2023-09-03 02:03:30 +0300

    solve rust/medium/yacht

--- rust/src/medium/alphametics.rs
+++ rust/src/medium/alphametics.rs
@@ -1,5 +1,4 @@
 use std::collections::{HashMap, HashSet};
-use std::iter::successors;
 
 #[derive(Debug)]
 struct Puzzle<'a> {
@@ -23,26 +22,7 @@ impl<'a> Puzzle<'a> {
     }
 }
 
-// FIXME: mutate array
-fn combinations(n: usize) -> impl Iterator<Item = Vec<u8>> {
-    // let init: Vec<u8> = (0..=9).take(n).collect();
-
-    // successors(Some(init), |v| {
-    //     let next_change_index = v.iter().rposition(|&num| num < 9)?;
-    //
-    //     Some(
-    //         v.into_iter()
-    //             .enumerate()
-    //             .map(|(ind, num)| match ind {
-    //                 ind if ind > next_change_index => 0,
-    //                 ind if ind == next_change_index => num + 1,
-    //                 _ => *num,
-    //             })
-    //             .collect(),
-    //     )
-    // })
-    let mut array: [u8; 9] = [0; 9];
-
+fn combinations(n: usize) {
     todo!()
 }
 
@@ -54,43 +34,7 @@ pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
         .filter(|char| char.is_ascii_alphabetic())
         .collect();
 
-    // FIXME
-    let solution = combinations(chars.len()).find(|set| {
-        if set.len() != set.iter().cloned().collect::<HashSet<u8>>().len() {
-            return false;
-        }
-
-        let map: HashMap<char, u8> = chars.iter().cloned().zip(set.iter().cloned()).collect();
-
-        let sum: usize = puzzle
-            .sum
-            .iter()
-            .map(|term| {
-                term.iter().enumerate().fold(0, |acc, (ind, x)| {
-                    acc + (map[x] as usize) * (10usize.pow(ind as u32))
-                })
-            })
-            .sum();
-
-        let result = puzzle
-            .result
-            .chars()
-            .rev()
-            .enumerate()
-            .fold(0, |acc, (ind, x)| {
-                acc + (map[&x] as usize) * (10usize.pow(ind as u32))
-            });
-
-        sum == result
-    })?;
-
-    Some(
-        chars
-            .iter()
-            .cloned()
-            .zip(solution.iter().cloned())
-            .collect(),
-    )
+    todo!();
 }
 
 #[cfg(test)]
@@ -110,63 +54,11 @@ mod tests {
         assert_alphametic_solution_eq("I + BB == ILL", &[('I', 1), ('B', 9), ('L', 0)]);
     }
 
-    // #[test]
-    // fn test_six_letters() {
-    //     assert_alphametic_solution_eq(
-    //         "NO + NO + TOO == LATE",
-    //         &[('N', 7), ('O', 4), ('T', 9), ('L', 1), ('A', 0), ('E', 2)],
-    //     );
-    // }
-    //
-    // #[test]
-    // fn test_puzzle_with_seven_letters() {
-    //     assert_alphametic_solution_eq(
-    //         "HE + SEES + THE == LIGHT",
-    //         &[
-    //             ('E', 4),
-    //             ('G', 2),
-    //             ('H', 5),
-    //             ('I', 0),
-    //             ('L', 1),
-    //             ('S', 9),
-    //             ('T', 7),
-    //         ],
-    //     );
-    // }
-    //
-    // #[test]
-    // fn test_puzzle_with_eight_letters() {
-    //     assert_alphametic_solution_eq(
-    //         "SEND + MORE == MONEY",
-    //         &[
-    //             ('S', 9),
-    //             ('E', 5),
-    //             ('N', 6),
-    //             ('D', 7),
-    //             ('M', 1),
-    //             ('O', 0),
-    //             ('R', 8),
-    //             ('Y', 2),
-    //         ],
-    //     );
-    // }
-    //
-    // #[test]
-    // fn test_puzzle_with_ten_letters_and_199_addends() {
-    //     assert_alphametic_solution_eq(
-    //         "THIS + A + FIRE + THEREFORE + FOR + ALL + HISTORIES + I + TELL + A + TALE + THAT + FALSIFIES + ITS + TITLE + TIS + A + LIE + THE + TALE + OF + THE + LAST + FIRE + HORSES + LATE + AFTER + THE + FIRST + FATHERS + FORESEE + THE + HORRORS + THE + LAST + FREE + TROLL + TERRIFIES + THE + HORSES + OF + FIRE + THE + TROLL + RESTS + AT + THE + HOLE + OF + LOSSES + IT + IS + THERE + THAT + SHE + STORES + ROLES + OF + LEATHERS + AFTER + SHE + SATISFIES + HER + HATE + OFF + THOSE + FEARS + A + TASTE + RISES + AS + SHE + HEARS + THE + LEAST + FAR + HORSE + THOSE + FAST + HORSES + THAT + FIRST + HEAR + THE + TROLL + FLEE + OFF + TO + THE + FOREST + THE + HORSES + THAT + ALERTS + RAISE + THE + STARES + OF + THE + OTHERS + AS + THE + TROLL + ASSAILS + AT + THE + TOTAL + SHIFT + HER + TEETH + TEAR + HOOF + OFF + TORSO + AS + THE + LAST + HORSE + FORFEITS + ITS + LIFE + THE + FIRST + FATHERS + HEAR + OF + THE + HORRORS + THEIR + FEARS + THAT + THE + FIRES + FOR + THEIR + FEASTS + ARREST + AS + THE + FIRST + FATHERS + RESETTLE + THE + LAST + OF + THE + FIRE + HORSES + THE + LAST + TROLL + HARASSES + THE + FOREST + HEART + FREE + AT + LAST + OF + THE + LAST + TROLL + ALL + OFFER + THEIR + FIRE + HEAT + TO + THE + ASSISTERS + FAR + OFF + THE + TROLL + FASTS + ITS + LIFE + SHORTER + AS + STARS + RISE + THE + HORSES + REST + SAFE + AFTER + ALL + SHARE + HOT + FISH + AS + THEIR + AFFILIATES + TAILOR + A + ROOFS + FOR + THEIR + SAFE == FORTRESSES",
-    //         &[
-    //         ('A', 1),
-    //         ('E', 0),
-    //         ('F', 5),
-    //         ('H', 8),
-    //         ('I', 7),
-    //         ('L', 2),
-    //         ('O', 6),
-    //         ('R', 3),
-    //         ('S', 4),
-    //         ('T', 9)
-    //         ],
-    //     );
-    // }
+    #[test]
+    fn test_six_letters() {
+        assert_alphametic_solution_eq(
+            "NO + NO + TOO == LATE",
+            &[('N', 7), ('O', 4), ('T', 9), ('L', 1), ('A', 0), ('E', 2)],
+        );
+    }
 }
--- rust/src/medium/yacht.rs
+++ rust/src/medium/yacht.rs
@@ -14,8 +14,64 @@ pub enum Category {
 }
 
 type Dice = [u8; 5];
-pub fn score(_dice: Dice, _category: Category) -> u8 {
-    unimplemented!("Solve the Yacht exercise");
+pub fn score(dice: Dice, category: Category) -> u8 {
+    use Category::*;
+
+    match category {
+        Ones => count(1, dice),
+        Twos => count(2, dice),
+        Threes => count(3, dice),
+        Fours => count(4, dice),
+        Fives => count(5, dice),
+        Sixes => count(6, dice),
+        FullHouse => {
+            let dices = (1..=6)
+                .map(|x| (dice.iter().filter(|i| **i == x)).count())
+                .filter(|x| *x == 2 || *x == 3)
+                .sum();
+
+            match dices {
+                5 => dice.iter().sum(),
+                _ => 0,
+            }
+        }
+        FourOfAKind => {
+            let kind = (1..=6).find(|x| (dice.iter().filter(|i| **i == *x)).count() >= 4);
+            match kind {
+                Some(kind) => dice.iter().filter(|i| **i == kind).take(4).sum(),
+                None => 0,
+            }
+        }
+        LittleStraight => {
+            let mut dice = dice.to_vec();
+            dice.sort_unstable();
+            dice.dedup();
+
+            match &dice[..] {
+                [1, 2, 3, 4, 5] => 30,
+                _ => 0,
+            }
+        }
+        BigStraight => {
+            let mut dice = dice.to_vec();
+            dice.sort_unstable();
+            dice.dedup();
+
+            match &dice[..] {
+                [2, 3, 4, 5, 6] => 30,
+                _ => 0,
+            }
+        }
+        Choice => dice.iter().sum(),
+        Yacht => match dice.iter().all(|item| *item == dice[0]) {
+            true => 50,
+            false => 0,
+        },
+    }
+}
+
+fn count(number: u8, dice: Dice) -> u8 {
+    number * dice.iter().filter(|x| **x == number).count() as u8
 }
 
 #[test]
@@ -23,4 +79,18 @@ fn test_yacht() {
     let expected = 50;
 
     assert_eq!(score([5, 5, 5, 5, 5], Category::Yacht), expected);
+}
+
+#[test]
+fn test_four_of_a_kind() {
+    let expected = 24;
+
+    assert_eq!(score([6, 6, 4, 6, 6], Category::FourOfAKind), expected);
+}
+
+#[test]
+fn test_four_of_a_kind_is_not_a_full_house() {
+    let expected = 0;
+
+    assert_eq!(score([1, 4, 4, 4, 4], Category::FullHouse), expected);
 }
