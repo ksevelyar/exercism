e5d13a883c13404b5ee23b865f71177d1234b199
Sergey Zubkov <ksevelyar@protonmail.com>
2023-10-15 14:05:38 +0300

    solve elixir/medium/allergies

---
+++ elixir/lib/medium/allergies.ex
@@ -0,0 +1,30 @@
+defmodule Allergies do
+  import Bitwise
+
+  @scores %{
+    "eggs" => 1,
+    "peanuts" => 2,
+    "shellfish" => 4,
+    "strawberries" => 8,
+    "tomatoes" => 16,
+    "chocolate" => 32,
+    "pollen" => 64,
+    "cats" => 128
+  }
+
+  @doc """
+  List the allergies for which the corresponding flag bit is true.
+  """
+  @spec list(non_neg_integer) :: [String.t()]
+  def list(flags) do
+    for {allergen, _} <- @scores, allergic_to?(flags, allergen), do: allergen
+  end
+
+  @doc """
+  Returns whether the corresponding flag bit in 'flags' is set for the item.
+  """
+  @spec allergic_to?(non_neg_integer, String.t()) :: boolean
+  def allergic_to?(flags, allergen) do
+    (@scores[allergen] &&& flags) > 0
+  end
+end
