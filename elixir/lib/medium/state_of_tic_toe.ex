fbb410b6fa7844c23faaf7cab69d05b7279d38a4
Sergey Zubkov <ksevelyar@protonmail.com>
2023-10-25 22:20:54 +0300

    add elixir/medium/transponse

--- elixir/lib/medium/state_of_tic_toe.ex
+++ elixir/lib/medium/state_of_tic_toe.ex
@@ -4,5 +4,26 @@ defmodule StateOfTicTacToe do
   """
   @spec game_state(board :: String.t()) :: {:ok, :win | :ongoing | :draw} | {:error, String.t()}
   def game_state(board) do
+    board = """
+    XOO
+    X..
+    X..
+    """
+  end
+
+  defp invalid_state?(board) do
+
+  end
+
+  defp win?(board) do
+    # check vertical lines
+    # check horizontal lines
+
+    {:ok, :win}
+  end
+
+  defp draw?(board) do
+    # check no empty cells available
+    {:ok, :draw}
   end
 end
---
+++ elixir/lib/medium/transponse.ex
@@ -0,0 +1,38 @@
+defmodule Transpose do
+  @doc """
+  Given an input text, output it transposed.
+
+  Rows become columns and columns become rows. See https://en.wikipedia.org/wiki/Transpose.
+
+  If the input has rows of different lengths, this is to be solved as follows:
+    * Pad to the left with spaces.
+    * Don't pad to the right.
+
+  ## Examples
+
+    iex> Transpose.transpose("ABC\\nDE")
+    "AD\\nBE\\nC"
+
+    iex> Transpose.transpose("AB\\nDEF")
+    "AD\\nBE\\n F"
+  """
+
+  @spec transpose(String.t()) :: String.t()
+  def transpose(input) do
+    lines = input |> String.split("\n") |> Enum.map(&String.graphemes/1)
+
+    longest_line_length = Enum.max_by(lines, &length(&1)) |> length()
+
+    lines = Enum.map(lines, fn line -> pad_line(line, longest_line_length) end)
+
+    0..(longest_line_length - 1)
+    |> Enum.map(fn row ->
+      lines |> Enum.map(fn line -> Enum.at(line, row) end) |> Enum.join() |> String.trim_trailing(" ")
+    end)
+    |> Enum.join("\n")
+  end
+
+  defp pad_line(line, longest_line_length) do
+    line ++ List.duplicate(" ", longest_line_length - length(line))
+  end
+end
