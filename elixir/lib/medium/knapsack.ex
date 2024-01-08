251277ecf73c5540fa8b37caf5002cbdd9dc04ab
Sergey Zubkov <ksevelyar@protonmail.com>
2024-01-08 19:11:31 +0300

    solve elixir/medium/knapsack

--- elixir/lib/medium/knapsack.ex
+++ elixir/lib/medium/knapsack.ex
@@ -1,23 +1,33 @@
 defmodule Knapsack do
-  def maximum_value(items, weight) do
-    acc = Enum.reduce(items, %{0 => 0}, fn item, acc ->
-      iterate(weight - item.weight, item, acc)
-    end)
+  def maximum_value([], _max_weight), do: 0
 
-    acc[weight] || 0
+  def maximum_value(items, max_weight) do
+    cache =
+      Enum.reduce(1..length(items), %{}, fn item_index, cache ->
+        iterate(item_index, items, max_weight, cache)
+      end)
+
+    cache[length(items)][max_weight] || 0
   end
 
-  defp iterate(max_weight, item, acc) do
-    Enum.reduce(0..max_weight, acc, fn weight, acc ->
-      if best_variant?(acc[weight], acc[weight + item.weight]) do
-        Map.put(acc, weight + item.weight, acc[weight] + item.value)
-      else
-        acc
-      end
-    end)
-  end
+  defp iterate(item_index, items, max_weight, cache) do
+    item = Enum.at(items, item_index - 1)
 
-  defp best_variant?(cache, weight) do
-    cache != nil && (weight == nil || weight < cache)
+    Enum.reduce(0..max_weight, cache, fn weight, cache ->
+      value_without_item = cache[item_index - 1][weight] || 0
+
+      value_with_item =
+        if weight >= item.weight do
+          remainder_value = cache[item_index - 1][weight - item.weight] || 0
+
+          item.value + remainder_value
+        else
+          0
+        end
+
+      max_value = max(value_without_item, value_with_item)
+
+      put_in(cache, [Access.key(item_index, %{}), weight], max_value)
+    end)
   end
 end
