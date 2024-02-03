a05b4fee7e03c9ad668874ca9913be0e7c020ebf
Sergey Zubkov <ksevelyar@protonmail.com>
2024-02-04 01:04:45 +0300

    fix elixir warnings

--- elixir/lib/medium/state_of_tic_toe.ex
+++ elixir/lib/medium/state_of_tic_toe.ex
@@ -3,26 +3,6 @@ defmodule StateOfTicTacToe do
   Determine the state a game of tic-tac-toe where X starts.
   """
   @spec game_state(board :: String.t()) :: {:ok, :win | :ongoing | :draw} | {:error, String.t()}
-  def game_state(board) do
-    board = """
-    XOO
-    X..
-    X..
-    """
-  end
-
-  defp invalid_state?(board) do
-  end
-
-  defp win?(board) do
-    # check vertical lines
-    # check horizontal lines
-
-    {:ok, :win}
-  end
-
-  defp draw?(board) do
-    # check no empty cells available
-    {:ok, :draw}
+  def game_state(_board) do
   end
 end
