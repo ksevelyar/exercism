1e81d9e83337d8b9be4cf1901b9dfaf04eb1c9d6
Sergey Zubkov <ksevelyar@protonmail.com>
2024-05-17 00:03:09 +0300

    add flake.nix for elixir

---
+++ elixir/flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "exercism-elixir";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        beamPackages = pkgs.beam.packagesWith pkgs.beam.interpreters.erlang;
+        elixir = beamPackages.elixir;
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            elixir
+            pkgs.elixir_ls
+
+            pkgs.inotify-tools
+          ];
+
+          hooks = ''
+            # this allows mix to work on the local directory
+            mkdir -p .nix-mix .nix-hex
+            export MIX_HOME=$PWD/.nix-mix
+            export HEX_HOME=$PWD/.nix-mix
+            export PATH=$MIX_HOME/bin:$HEX_HOME/bin:$PATH
+
+            export ERL_AFLAGS="-kernel shell_history enabled"
+          '';
+        };
+      });
+}
--- elixir/mix.exs
+++ elixir/mix.exs
@@ -5,7 +5,7 @@ defmodule Exercism.MixProject do
     [
       app: :exercism,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
