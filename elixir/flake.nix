588cbbf899719d8bb6907b5e97000326a1efc77f
Sergey Zubkov <ksevelyar@protonmail.com>
2024-05-25 13:23:45 +0300

    OTP 27

--- elixir/flake.nix
+++ elixir/flake.nix
@@ -1,36 +1,36 @@
 {
   description = "exercism-elixir";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
 
-        beamPackages = pkgs.beam.packagesWith pkgs.beam.interpreters.erlang;
-        elixir = beamPackages.elixir;
-      in
-      {
-        devShell = pkgs.mkShell {
-          buildInputs = [
-            elixir
-            pkgs.elixir_ls
+      beamPackages = pkgs.beam.packagesWith pkgs.beam.interpreters.erlang_27;
+      elixir = beamPackages.elixir_1_16;
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = [
+          elixir
+          pkgs.elixir_ls
+          pkgs.inotify-tools
+          pkgs.nixpkgs-fmt
+        ];
 
-            pkgs.inotify-tools
-          ];
+        hooks = ''
+          # this allows mix to work on the local directory
+          mkdir -p .nix-mix .nix-hex
+          export MIX_HOME=$PWD/.nix-mix
+          export HEX_HOME=$PWD/.nix-mix
+          export PATH=$MIX_HOME/bin:$HEX_HOME/bin:$PATH
 
-          hooks = ''
-            # this allows mix to work on the local directory
-            mkdir -p .nix-mix .nix-hex
-            export MIX_HOME=$PWD/.nix-mix
-            export HEX_HOME=$PWD/.nix-mix
-            export PATH=$MIX_HOME/bin:$HEX_HOME/bin:$PATH
-
-            export ERL_AFLAGS="-kernel shell_history enabled"
-          '';
-        };
-      });
+          export ERL_AFLAGS="-kernel shell_history enabled"
+        '';
+      };
+    });
 }
